786f7ac5c0901d6c794f1ff0fa1a753667d28cb0
ItzRozzaDev <66577157+ItzRozzaDev@users.noreply.github.com>
2022-05-28 18:47:48 +0100

    Update Replacer.java

--- src/main/java/dev/itzrozzadev/core/text/Replacer.java
+++ src/main/java/dev/itzrozzadev/core/text/Replacer.java
@@ -2,11 +2,22 @@ package dev.itzrozzadev.core.text;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @UtilityClass
 public class Replacer {
+
+	public List<String> replace(final List<String> text, final String... replacements) {
+		final List<String> replacedText = new ArrayList<>();
+
+		for (final String string : text) {
+			replacedText.add(replace(string, replacements));
+		}
+		return replacedText;
+	}
 
 	public String replace(String text, final String... replacements) {
 		final Map<String, String> map = new HashMap<>();
